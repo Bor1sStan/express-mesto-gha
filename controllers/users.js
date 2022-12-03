d99bbf82dfa1ddf03f01eea802a9d79a10001703
Boris Stan <s9110039682@yandex.ru>
2022-12-03 21:37:44 +0700

    feat/try 11

--- controllers/users.js
+++ controllers/users.js
@@ -1,121 +1,89 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const User = require('../models/user');
 
-const ERROR_CODE = 500;
-const ERROR_DATA_CODE = 400;
-const NOT_FOUND_CODE = 404;
-
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch(() => res.status(ERROR_CODE).send({ message: 'Произошла ошибка' }));
+// eslint-disable-next-line consistent-return
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    return res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
 };
 
 const getUserById = (req, res) => {
-  User.findById(req.params.userId)
-    .then((user) => {
-      if (!user) {
-        return res.status(NOT_FOUND_CODE).send({ message: 'Пользователь не найден' });
-      }
-      return res.send({ data: user });
-    })
+  User.findById(req.params.userId).orFail(new Error('NotFound'))
+    .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ERROR_DATA_CODE).send({ message: 'Ошибка валидации. Переданные данные не корректны' });
+      if (err.message === 'NotFound') {
+        return res.status(404).send({ message: 'Пользователь не найден' });
       }
-      return res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(400).send({ message: 'Некоректный id' });
+      }
+      return res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
 };
-
-// const getUserById = (req, res) => {
-//   User.findById(req.params.userId).orFail(new Error('NotFound'))
-//     .then((user) => res.send({ data: user }))
-//     .catch((err) => {
-//       if (err.message === 'NotFound') {
-//         return res.status(NOT_FOUND_CODE).send('Пользователь не найден');
-//       }
-//       if (err instanceof mongoose.Error.CastError) {
-//         return res.status(ERROR_DATA_CODE).send({ message: 'Не коректный id', err });
-//       }
-//       return res.status(ERROR_CODE).send({ message: 'На сервере произошла ошибка' });
-//     });
-// };
 
 const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
+  const {
+    name,
+    about,
+    avatar,
+  } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((e) => {
-      if (e.name === 'ValidationError') {
-        return res.status(ERROR_DATA_CODE).send({
-          message: 'Ошибка валидации. Переданные данные не корректны',
-        });
+    .then((user) => res.status(201).send({ data: user }))
+    .catch((err) => {
+      if (err instanceof mongoose.Error.CastError) {
+        return res.status(400).send({ message: 'Ошибка валидации' });
       }
-      return res
-        .status(ERROR_CODE)
-        .send({ message: 'Произошла ошибка на сервере' });
+      return res.status(500).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
-const updateUserProfile = (req, res) => {
-  const { name, about } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    { new: true, runValidators: true },
-  )
-    .then((user) => {
-      if (!user) {
-        return res
-          .status(NOT_FOUND_CODE)
-          .send({ message: 'Пользователь не найден' });
-      }
-      return res.send({ user });
-    })
-    .catch((e) => {
-      if (e.name === 'CastError" || e.name === "ValidationError') {
-        return res.status(ERROR_DATA_CODE).send({
-          message: 'Ошибка валидации. Переданные данные не корректны',
-        });
-      }
-      return res
-        .status(ERROR_CODE)
-        .send({ message: 'Произошла ошибка на сервере' });
-    });
+// eslint-disable-next-line consistent-return
+const updateUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).send('Пользователь не найден');
+    }
+    const { name, about } = req.body;
+    // eslint-disable-next-line max-len
+    const newUser = await User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true });
+    res.send(newUser);
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError || err instanceof mongoose.Error.CastError) {
+      return res.status(400).send({ message: 'Ошибка валидации. Переданные данные не корректны' });
+    }
+    return res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
 };
 
-const updateUserAvatar = (req, res) => {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(
-    req.user._id,
-    { avatar },
-    { new: true, runValidators: true },
-  )
-    .then((user) => {
-      if (!user) {
-        return res
-          .status(NOT_FOUND_CODE)
-          .send({ message: 'Пользователь не найден' });
-      }
-      return res.send({ user });
-    })
-    .catch((e) => {
-      if (e.name === 'CastError' || e.name === 'ValidationError') {
-        return res.status(ERROR_DATA_CODE).send({
-          message: 'Ошибка валидации. Переданные данные не корректны',
-        });
-      }
-      return res
-        .status(ERROR_CODE)
-        .send({ message: 'Произошла ошибка на сервере' });
-    });
+// eslint-disable-next-line consistent-return
+const updateAvatar = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      return res.status(404).send('Пользователь не найден');
+    }
+    const { avatar } = req.body;
+    // eslint-disable-next-line max-len
+    const newAvatar = User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidation: true });
+    res.send(newAvatar);
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError || err instanceof mongoose.Error.CastError) {
+      res.status(400).send({ message: 'Ошибка валидации. Переданные данные не корректны' });
+    }
+    return res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
 };
 
 module.exports = {
   getUsers,
   getUserById,
   createUser,
-  updateUserProfile,
-  updateUserAvatar,
+  updateUser,
+  updateAvatar,
 };
