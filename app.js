d9b9e44a0e7e9d4f2e74b35a5b5e9aeb083d8d68
Boris Stan <s9110039682@yandex.ru>
2022-12-22 22:33:48 +0700

    feat/try again4

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
+
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
